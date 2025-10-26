dcf5853a51460cf6bd2f2c903e9ea6b29a74e578
jehad630 <devjehad630@gmail.com>
2025-10-26 17:13:21 +0300

    make the transition from splash view to home view and create the home view

--- lib/features/splash/presentation/views/widgets/SplashViewBody.dart
+++ lib/features/splash/presentation/views/widgets/SplashViewBody.dart
@@ -1,6 +1,10 @@
+import 'package:booklyapp/constants.dart';
 import 'package:booklyapp/core/utils/assets.dart';
+import 'package:booklyapp/features/home/presentation/views/home_view.dart';
 import 'package:booklyapp/features/splash/presentation/views/widgets/SlidingTextAnimation.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 
 class Splashviewbody extends StatefulWidget {
   const Splashviewbody({super.key});
@@ -13,10 +17,39 @@ class _SplashviewbodyState extends State<Splashviewbody>
     with SingleTickerProviderStateMixin {
   late AnimationController animationController;
   late Animation<Offset> sildingAnimtion;
+
   @override
   void initState() {
     super.initState();
+    // animation method
+    initSlidingAnimation();
+    // transition method
+    NavigateToHomeView();
+  }
 
+  @override
+  void dispose() {
+    super.dispose();
+    //adding dispose for animationController
+    animationController.dispose();
+  }
+
+  // widgets
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(AssetsData.logo),
+        SizedBox(height: 4),
+        SlidingTextAnimation(sildingAnimtion: sildingAnimtion),
+      ],
+    );
+  }
+
+  // animation method
+  void initSlidingAnimation() {
     animationController = AnimationController(
       vsync: this,
       duration: Duration(seconds: 1),
@@ -29,22 +62,14 @@ class _SplashviewbodyState extends State<Splashviewbody>
     animationController.forward();
   }
 
-  @override
-  void dispose() {
-    super.dispose();
-    animationController.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Image.asset(AssetsData.logo),
-        SizedBox(height: 4),
-        SlidingTextAnimation(sildingAnimtion: sildingAnimtion),
-      ],
-    );
+  // transition method
+  void NavigateToHomeView() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Get.to(
+        () => const HomeView(),
+        transition: Transition.fade,
+        duration: kTranstionDuration,
+      );
+    });
   }
 }
