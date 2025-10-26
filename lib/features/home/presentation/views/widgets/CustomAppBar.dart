0120cf33b1ddd834ab750d1783e772a0b0f8b574
jehad630 <devjehad630@gmail.com>
2025-10-26 17:33:20 +0300

    bulding app bar for the home view

---
+++ lib/features/home/presentation/views/widgets/CustomAppBar.dart
@@ -0,0 +1,24 @@
+import 'package:booklyapp/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 32),
+      child: Row(
+        children: [
+          Image.asset(AssetsData.logo, height: 24),
+          Spacer(),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(FontAwesomeIcons.magnifyingGlass, size: 32),
+          ),
+        ],
+      ),
+    );
+  }
+}
