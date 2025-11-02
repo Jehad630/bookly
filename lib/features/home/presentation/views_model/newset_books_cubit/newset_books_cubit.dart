84263a59e2ce1822ff491be491f9d98ffb866cb1
jehad630 <devjehad630@gmail.com>
2025-11-02 18:22:10 +0300

    create news books cubit & state

---
+++ lib/features/home/presentation/views_model/newset_books_cubit/newset_books_cubit.dart
@@ -0,0 +1,26 @@
+import 'package:bloc/bloc.dart';
+import 'package:booklyapp/features/home/data/models/books_model/books_model.dart';
+import 'package:booklyapp/features/home/data/repo/home_repo.dart';
+import 'package:equatable/equatable.dart';
+
+part 'newset_books_state.dart';
+
+class NewsetBooksCubit extends Cubit<NewsetBooksState> {
+  NewsetBooksCubit(this.homeRepo) : super(NewsetBooksInitial());
+  
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewsetBooks() async {
+    emit(NewsetBooksLoading());
+    var result = await homeRepo.fetchNewstBooks();
+
+    result.fold(
+      (faliure) {
+        emit(NewsetBooksFaliure(faliure.errmesg));
+      },
+      (books) {
+        emit(NewsetBooksSuccess(books));
+      },
+    );
+  }
+}
---
+++ lib/features/home/presentation/views_model/newset_books_cubit/newset_books_state.dart
@@ -0,0 +1,19 @@
+part of 'newset_books_cubit.dart';
+
+sealed class NewsetBooksState extends Equatable {
+  const NewsetBooksState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class NewsetBooksInitial extends NewsetBooksState {}
+final class NewsetBooksLoading extends NewsetBooksState {}
+final class NewsetBooksSuccess extends NewsetBooksState {
+  final List<BooksModel> books;
+  NewsetBooksSuccess(this.books);
+}
+final class NewsetBooksFaliure extends NewsetBooksState {
+  final String errmesg;
+const  NewsetBooksFaliure(this.errmesg);
+}
