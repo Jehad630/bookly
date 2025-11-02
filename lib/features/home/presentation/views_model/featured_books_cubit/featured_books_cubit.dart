d87729320dd8827f3c5669262613f6acfb6548a7
jehad630 <devjehad630@gmail.com>
2025-11-02 18:17:09 +0300

    create feaatured books method

--- lib/features/home/presentation/views_model/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/views_model/featured_books_cubit/featured_books_cubit.dart
@@ -1,9 +1,26 @@
 import 'package:bloc/bloc.dart';
 import 'package:booklyapp/features/home/data/models/books_model/books_model.dart';
+import 'package:booklyapp/features/home/data/repo/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+
+    result.fold(
+      (faliure) {
+        emit(FeaturedBooksFailure(faliure.errmesg));
+      },
+      (books) {
+        emit(FeaturedBooksSuccess(books));
+      },
+    );
+  }
 }
