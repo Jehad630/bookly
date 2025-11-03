17d9940ab36198b4e16405d64737d53a458b8339
jehad630 <devjehad630@gmail.com>
2025-11-03 13:52:08 +0300

    fetch similer books

--- lib/features/home/data/repo/home_repo_impl.dart
+++ lib/features/home/data/repo/home_repo_impl.dart
@@ -2,6 +2,7 @@ import 'package:booklyapp/core/errors/Failure.dart';
 import 'package:booklyapp/core/utils/api_service.dart';
 import 'package:booklyapp/features/home/data/models/books_model/books_model.dart';
 import 'package:booklyapp/features/home/data/repo/home_repo.dart';
+import 'package:booklyapp/features/home/presentation/views_model/featured_books_cubit/featured_books_cubit.dart';
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 
@@ -40,6 +41,29 @@ class HomeRepoImpl implements HomeRepo {
     try {
       var data = await apiService.get(
         endpoint: "/volumes?Filtering=free-ebooks&q=subject:Programming",
+      );
+      List<BooksModel> books = [];
+      for (var item in data["items"]) {
+        books.add(BooksModel.fromjson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(serverFaliure.fromDioError(e));
+      }
+
+      return left(serverFaliure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<BooksModel>>> fetchSimilerBooks({
+    required String category,
+  }) async {
+    try {
+      var data = await apiService.get(
+        endpoint:
+            "/volumes?Filtering=free-ebooks&q=subject:Computer science&sorting=relevance",
       );
       List<BooksModel> books = [];
       for (var item in data["items"]) {
